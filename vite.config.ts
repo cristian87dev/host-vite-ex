292bf35597e180c33d5025ef4f54fdd05f374480
Cristian Perrone <cristian.perrone@dedalord.com>
2024-03-24 11:47:27 -0300

    add github pages domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://cristianperrone.dev',
+  base: 'https://cristian87dev.github.io/host-vite-ex', //'https://cristianperrone.dev',
 })
