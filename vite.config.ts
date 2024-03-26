add7c48806372950a1717abb1f87d3f934f1d080
Cristian Esteban Perrone <cristianperrone.dev@gmail.com>
2024-03-25 23:00:04 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.cristianperrone.dev', //'https://cristian87dev.github.io/host-vite-ex'
+  base: 'https://github.cristianperrone.com', //'https://cristian87dev.github.io/host-vite-ex'
 })
